69f82f81e19ff9216c3ecc0575a53b6081b163ab
mayapm18 <ngo.h.mai.18@gmail.com>
2025-10-23 01:23:29 +0300

    employee id

---
+++ SQL/join_n_using.sql
@@ -0,0 +1,6 @@
+#https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT unique_id, name
+FROM Employees
+LEFT JOIN EmployeeUNI
+USING (id)
