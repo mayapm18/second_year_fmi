ec0e3297e288f9f82c6438658acff66066f13f52
mayapm18 <ngo.h.mai.18@gmail.com>
2025-10-29 23:27:18 +0200

    WHERE NOT IN

--- SQL/visits_transaction.sql
+++ SQL/visits_transaction.sql
@@ -1,8 +1,8 @@
 # https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/?envType=study-plan-v2&envId=top-sql-50
 
-SELECT customer_id, count_no_trans
+SELECT customer_id, visit_id
 FROM Visits
-JOIN Transaction
-USING (visit_id)
-
+WHERE visit_id NOT IN 
+SELECT visit_id
+FROM Transactions
 #?????
