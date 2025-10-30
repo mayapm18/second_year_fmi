17d703e2d8ed8871d8b6142d72fb263fe7cd0f3d
mayapm18 <ngo.h.mai.18@gmail.com>
2025-10-30 18:17:41 +0200

    count_no_trans

--- SQL/visits_transaction.sql
+++ SQL/visits_transaction.sql
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/?envType=study-plan-v2&envId=top-sql-50
 
-SELECT customer_id, visit_id
+SELECT customer_id, count_no_trans
 FROM Visits
 WHERE visit_id NOT IN 
 SELECT visit_id
