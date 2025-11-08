b7810bd5a809b5054b4f6718192d0173a8813a15
mayapm18 <ngo.h.mai.18@gmail.com>
2025-11-08 22:56:18 +0200

    Group by, NOT IN

--- SQL/visits_transaction.sql
+++ SQL/visits_transaction.sql
@@ -1,8 +1,14 @@
 # https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/?envType=study-plan-v2&envId=top-sql-50
 
-SELECT customer_id, count_no_trans
+SELECT customer_id, COUNT(visit_id) AS count_no_trans
 FROM Visits
 WHERE visit_id NOT IN 
+( 
 SELECT visit_id
 FROM Transactions
-#?????
+)
+#Keep only those visits that are not found in the Transactions table.
+GROUP BY customer_id
+
+#GROUP BY collects rows that have the same value into one group
+#so you can count, sum, or average them.
