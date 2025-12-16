edf39c70c11445b3120916d0175e2abae2190beb
mayapm18 <ngo.h.mai.18@gmail.com>
2025-12-16 18:09:09 +0200

    fix

--- SQL/confirmation _rate.sql
+++ SQL/confirmation _rate.sql
@@ -1,12 +1,8 @@
 # https://leetcode.com/problems/confirmation-rate/description/?envType=study-plan-v2&envId=top-sql-50
 
-# https://leetcode.com/problems/confirmation-rate/description/?envType=study-plan-v2&envId=top-sql-50
-
-SELECT s.user_id, confirmation_rate
-FROM Signups
-RIGHT JOIN Confirmations
-WHERE (
-    confirmation_rate = COUNT(confirmed actions)/COUNT(all actions)
-    if no actons -> confirmation_rate=0
-    ROUND(confirmation_rate, 2)
-)
+SELECT s.user_id, ROUND(IFNULL(COUNT(CASE WHEN c.action = 'confirmed' THEN 1 END) / COUNT(c.action), 0), 2) AS confirmation_rate
+#IFNULL : when NULL = 0
+FROM Signups s
+LEFT JOIN Confirmations c #add B to A and if B doesn’t have a match, still keep the row from A
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
