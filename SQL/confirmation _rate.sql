dcf9db3fe038d47693e0b61468f463919df44b8c
mayapm18 <ngo.h.mai.18@gmail.com>
2025-12-16 17:57:19 +0200

    correct table name

--- SQL/confirmation _rate.sql
+++ SQL/confirmation _rate.sql
@@ -1,6 +1,12 @@
 # https://leetcode.com/problems/confirmation-rate/description/?envType=study-plan-v2&envId=top-sql-50
 
-SELECT s.user_id, ROUND(AVG(IF(c.action = 'confirmed',1,0)),2) AS confirmation_rate #confirmed = 1, others=0, then avg
-FROM Signups s
-RIGHT JOIN Confirmation c ON s.user_id = c.user_id #match rows from S and C -> user_id are equal
-GROUP BY s.user_id
+# https://leetcode.com/problems/confirmation-rate/description/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT s.user_id, confirmation_rate
+FROM Signups
+RIGHT JOIN Confirmations
+WHERE (
+    confirmation_rate = COUNT(confirmed actions)/COUNT(all actions)
+    if no actons -> confirmation_rate=0
+    ROUND(confirmation_rate, 2)
+)
