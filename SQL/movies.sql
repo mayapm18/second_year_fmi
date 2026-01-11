d95807db3e8efb48e15d80daa547c05e29760d7b
mayapm18 <ngo.h.mai.18@gmail.com>
2026-01-11 18:11:02 +0200

    ORDER BY rating DESC

---
+++ SQL/movies.sql
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
